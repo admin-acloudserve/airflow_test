651ce156e9d2c9c27350d507ef72e0e8a37cba0a
ziwa <106367778+zwang555@users.noreply.github.com>
2023-10-09 20:56:40 -0400

    Add files via upload

---
+++ mysql2clickhouse.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import mysql.connector
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 6, 14),
+    'retries': 1
+}
+
+dag = DAG(
+    'sample_dag',
+    default_args=default_args,
+    description='A simple Airflow DAG',
+    schedule_interval='once'
+)
+
+def get_connection():
+    mydb = mysql.connector.connect(
+        host="my-db-pxc-db-haproxy.xtradb.svc.cluster.local",
+        user="airflow_user",
+        password="airflow_pass",
+        database="airflow_db"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM job")
+
+    myresult = mycursor.fetchall()
+
+    print(type(myresult))
+    for x in myresult:
+        print(x)
+
+with dag:
+    extract_task = PythonOperator(
+        task_id='get_connection',
+        python_callable=get_connection
+    )
+    extract_task 
