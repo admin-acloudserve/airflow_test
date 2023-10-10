29b2387dadc80bcf162df6bf1c4aa1b91bce5262
ziwa <106367778+zwang555@users.noreply.github.com>
2023-10-09 21:00:30 -0400

    Update mysql2clickhouse.py

--- mysql2clickhouse.py
+++ mysql2clickhouse.py
@@ -26,7 +26,7 @@ def get_connection():
 
     mycursor = mydb.cursor()
 
-    mycursor.execute("SELECT * FROM job")
+    mycursor.execute("SELECT dag_id FROM dag")
 
     myresult = mycursor.fetchall()
 
